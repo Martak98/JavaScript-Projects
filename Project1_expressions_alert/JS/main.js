c4cc00c3741520df38ea0056ab346581245a36a1
Marta <marta.kusiak16@gmail.com>
2025-02-20 22:34:50 +0000

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello world!");                         //Displaying window alert
+
+document.write('Hello, world!');                      //Adding a text
+
+var Sent1 = "This is beggining of the srting";        //Adding a variable  
+var Sent2 = " and this is the end of the string";    
+document.write(Sent1+Sent2);                          //Displaying varaible 
+
+document.write(17+48);                                //Adding experssion
