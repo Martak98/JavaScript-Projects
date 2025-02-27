5e6419ca133762bd2f2147bd20cc40af645a86c8
Marta <marta.kusiak16@gmail.com>
2025-02-27 21:57:54 +0000

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+    //a function with use of +=
+    function myFunction() {
+    var sentence ="I am learing";
+    sentence +="a lot from this book!"; //using += operator
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementById
+  }
