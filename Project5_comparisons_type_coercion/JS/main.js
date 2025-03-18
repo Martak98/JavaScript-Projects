dc08997b92e0269f7fe3855330f46f24d6ef17ce
Marta <marta.kusiak16@gmail.com>
2025-03-18 14:48:48 +0000

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,32 @@
+//expression with string and number
+document.write (5>3);
+document.write ("<br>");
+document.write(3==3);
+document.write (5==9);
+//displaying results in console
+console.log(5+7);
+console.log(5<2);
+//Making comparision using tripple equal signs
+X = 8;
+Y = 8;
+ document.write (X===Y);
+ X = 46;
+ Y= "46";
+ document.write (X===Y);
+ X= "Andy";
+ Y = "Andy";
+ document.write (X===Y);
+ X = "Max";
+ Y = "Matt";
+ document.write (X===Y);
+ //utylizing and, or, not operators
+ document.write(8>4 && 27<39);
+ document.write(4>6 && 31<27);
+ document.write(9>5 || 67<97);
+ document.write (6<2 || 35>63);
+ function not_Function (){
+    document.getElementById("Not").innerHTML = !(5>2);
+ }
+ function not_Function (){
+    document.getElementById("Not").innerHTML=!(57<13);
+ }
